c6ea17739819fb8202ee808acea7860d3e17cbba
Markus Borg <markus.borg@gmail.com>
2017-12-13 07:52:18 +0100

    Add bearing tests for negative result and zero

--- project/rumble/basic-bots/source-code/etsa02_basicbots/test/BasicBots_UnitTests.java
+++ project/rumble/basic-bots/source-code/etsa02_basicbots/test/BasicBots_UnitTests.java
@@ -32,24 +32,34 @@ public class BasicBots_UnitTests {
 	public void testDetectTeammate() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void testDetectNewEnemy() {
 		fail("Not yet implemented");
-	}	
-	
+	}
+
 	@Test
 	public void testDetectSameEnemy() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void testCalculateRobotBearing() {
-		MockScannedRobotEvent e = new MockScannedRobotEvent("mockEnemy1", 100.0, 20.0, 20.0, 20.0, 20.0, false);	
+		// Test positive bearing
+		MockScannedRobotEvent e = new MockScannedRobotEvent("mockEnemy1", 100.0, 20.0, 20.0, 20.0, 20.0, false);
+		assertEquals("Check that bearing of scanned enemy is correctly calculated",
+					 110, radarUT.calculateRobotBearing(botUT.getHeading(), e), 0.01);
+		// Test negative bearing
+		MockScannedRobotEvent e2 = new MockScannedRobotEvent("mockEnemy2", 100.0, -180, 20.0, 20.0, 20.0, false);
+		System.out.println(botUT.getHeading() + " + " + e2.getBearing());
 		assertEquals("Check that bearing of scanned enemy is correctly calculated", 
-				 110, radarUT.calculateRobotBearing(botUT.getHeading(), e), 0.01);
+					 -90, radarUT.calculateRobotBearing(botUT.getHeading(), e2), 0.01);
+		// Test zero bearing
+		MockScannedRobotEvent e3 = new MockScannedRobotEvent("mockEnemy3", 100.0, -90.0, 20.0, 20.0, 20.0, false);
+		assertEquals("Check that bearing of scanned enemy is correctly calculated", 
+					 0, radarUT.calculateRobotBearing(botUT.getHeading(), e3), 0.01);
 	}
-	
+
 	@Test
 	public void testCalculateRobotPosition() {
 		fail("Not yet implemented");
