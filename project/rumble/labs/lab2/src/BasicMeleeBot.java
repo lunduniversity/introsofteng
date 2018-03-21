13d9dbe88ff65c43dd508e52d0758a5c34067658
Markus Borg <markus.borg@gmail.com>
2018-03-21 11:13:05 +0100

    Added a completed main robot class

--- project/rumble/labs/lab2/src/BasicMeleeBot.java
+++ project/rumble/labs/lab2/src/BasicMeleeBot.java
@@ -1,3 +1,27 @@
+/**	
+Copyright (c) 2018 David Phung
+
+Building on work by Mathew A. Nelson and Robocode contributors.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package etsa02_lab2;
 
 import robocode.AdvancedRobot;
@@ -5,21 +29,60 @@ import robocode.RobotDeathEvent;
 import robocode.ScannedRobotEvent;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+/**
+ * @author David Phung
+ * 
+ * Example robot for ETSA02. A melee bot implemented following an object oriented design.
+ */
 public class BasicMeleeBot extends AdvancedRobot{
 
+	private EnemyDatabase enemyDatabase;
+	private PositioningSystem positioningSystem;
+	private MovementSystem movementSystem;
+	private TargetingSystem targetingSystem;
 	
+	/**
+	 * The main loop controlling the robot behavior.
+	 */
 	@Override
 	public void run() {
-		throw new NotImplementedException();
+		enemyDatabase = new EnemyDatabase(this);
+		positioningSystem = new PositioningSystem(getBattleFieldWidth(), getBattleFieldHeight());
+		movementSystem = new MovementSystem(this, enemyDatabase, positioningSystem);
+		targetingSystem = new TargetingSystem(enemyDatabase, this);
+		
+		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForGunTurn(true);
+		
+		targetingSystem.update();
+		movementSystem.update();
+		execute();
+
+		while (true) {
+			setTurnRadarRight(Double.POSITIVE_INFINITY);
+			targetingSystem.update();
+			movementSystem.update();
+			execute();
+		}
 	}
 	
+	/**
+	 * Describes the action taken when a robot has been scanned.
+	 * 
+	 * @param event The ScannedRobotEvent provided by Robocode.
+	 */
 	@Override
 	public void onScannedRobot(ScannedRobotEvent event) {
-		throw new NotImplementedException();
+		enemyDatabase.addEnemy(event);
 	}
 	
+	/**
+	 * Describes the action taken when a robot has been destroyed.
+	 * 
+	 * @param event The RobotDeathEvent provided by Robocode.
+	 */
 	@Override
 	public void onRobotDeath(RobotDeathEvent event) {
-		throw new NotImplementedException();
+		enemyDatabase.removeEnemy(event.getName());
 	}
 }
